7f199533134c8d6e18f49deaa2372dd65109bf54
Hammad <hammadpat@gmail.com>
2021-12-28 15:55:39 -0500

    first proper change

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
-const adder = document.querySelector('.submitbutton');
-
-switcher.addEventListener('click',function() {
-    document.body.classList.toggle('dark-theme')
-
-    var className = document.body.className;
-    if(className == "light-theme") {
-        this.textContent = "Dark";
+//code for enter button if user is inside the input box
+const enterkey = document.getElementsByClassName("inputtext")[0];
+enterkey.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        changelistitem();
     }
-    else {
-        this.textContent = "Light";
-    }
-
-    console.log('current class name: ' + className);
-
-});
-
-adder.addEventListener('click',function() {
-    document.getElementById("firstitem").textContent = document.getElementById("thingy").value;
+})
+//if user clicks on textbox, clear the default text
+enterkey.addEventListener("click", function() {
+    document.getElementById("listadd").value = "";
 })
 
+//adds item to list using value in the input box
+function changelistitem() {
+    var node = document.createElement("LI");
+    var addition = document.getElementById("listadd").value;
+    var textnode = document.createTextNode(addition);
+    node.appendChild(textnode);
+    document.getElementById("mylist").appendChild(node);
+    isEmpty();
+    document.getElementById("listadd").value = "enter task here";
+    //get user cursor focus off textbox
+    document.getElementById("listadd").blur();
+}
+
+//checks if the list containing tasks is empty 
+function isEmpty() {
+    if ((document.getElementById("mylist").innerHTML.trim() == "") == false) {
+        //number of tasks
+        document.getElementById("tasks").textContent = "Current Tasks: " + document.getElementById("mylist").getElementsByTagName("li").length;
+    }
+    else {
+        document.getElementById("tasks").textContent = "Current Tasks: None";
+    }
+  }
+
+//clears all tasks
+  function clearitemlist() {
+    document.getElementById("mylist").innerHTML = "";
+    isEmpty();
+  }
